e78e2f21a34a1138fe9847d9731bc409d2e743f8
Succatash <mstein1284@gmail.com>
2020-08-16 12:43:49 -0400

    Updates

---
+++ precache-manifest.13da96ac1ea1d00b798f8b1aa3ad3302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd32097e65e136367f6dfc6c76e67838",
+    "url": "/Monsters-rolodex/index.html"
+  },
+  {
+    "revision": "84b001cda7760a5d0115",
+    "url": "/Monsters-rolodex/static/css/main.6d87f05e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b001cda7760a5d0115",
+    "url": "/Monsters-rolodex/static/js/main.53432cc1.chunk.js"
+  },
+  {
+    "revision": "187e6764a2c841b24bfe",
+    "url": "/Monsters-rolodex/static/js/runtime-main.8d598150.js"
+  }
+]);
